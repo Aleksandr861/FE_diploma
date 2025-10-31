46c2ffc74bd54cd4f82d71d34881c1ec2a0e5101
Aleksandr Mamontov <mamont861@yandex.ru>
2025-10-31 22:33:57 +0300

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![CI](https://github.com/Aleksandr861/FE_diploma/actions/workflows/web.yml/badge.svg)
+
+https://aleksandr861.github.io/FE_diploma/
---
+++ src/components/Copyrights/Copyrights.tsx
@@ -0,0 +1,16 @@
+import './copyrights.css';
+
+const Copyrights = () => {
+  return (
+    <a
+      className="copyrights"
+      href="https://github.com/Aleksandr861"
+      target="_blank"
+      title="github"
+    >
+      2025 Aleksandr Mamontov
+    </a>
+  );
+};
+
+export default Copyrights;
